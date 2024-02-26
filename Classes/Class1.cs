1bcde84f9ae68ba2b69b6086220e3098f791436d
admsho <admsho@gmail.com>
2024-02-26 08:27:17 +0200

    Add project files.

---
+++ Classes/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloClass.Classes
+{
+    internal class Class1
+    {
+        public string hello(string str)
+        {
+            return str;
+        }
+
+        public string Calc(int i, int k, string opertr)
+        { int result=0;
+            if (opertr == "+")
+                result = i + k;
+            if (opertr == "-")
+                result = i - k;
+            if (opertr == "*")
+                result = i * k;
+            else
+                return "Error,type one of the operators:*,-,+";
+
+            return result.ToString(); 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+HelloClass.Classes.Class1 cls1=new HelloClass.Classes.Class1();
+string st=cls1.hello("Hello, World!");
+Console.Write(st);
+
+//string read = Console.ReadLine();
+
+Console.Write(cls1.Calc(4, 2,"+"));
